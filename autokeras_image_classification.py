5e74b79d39bea68dc48a46930482aa242c1fec0b
astroeye <astroeye@hotmail.com>
2022-02-11 01:29:24 +0900

    commit-20220211-001

---
+++ autokeras_image_classification.py
@@ -0,0 +1,58 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import tensorflow as tf
+from tensorflow.keras.datasets import mnist, fashion_mnist
+
+import autokeras as ak
+
+#(x_train, y_train), (x_test, y_test) = mnist.load_data()
+(x_train, y_train), (x_test, y_test) = fashion_mnist.load_data()
+print(x_train.shape)  # (60000, 28, 28)
+print(y_train.shape)  # (60000,)
+print(y_train[:3])  # array([7, 2, 1], dtype=uint8)
+
+n = 10
+plt.figure(figsize=(20, 4))
+for i in range(n):
+  # display original
+  ax = plt.subplot(2, n, i + 1)
+  plt.imshow(x_train[i])
+  plt.title(y_train[i])
+  plt.gray()
+  ax.get_xaxis().set_visible(False)
+  ax.get_yaxis().set_visible(False)
+
+  # display reconstruction
+  ax = plt.subplot(2, n, i + 1 + n)
+  plt.imshow(x_test[i])
+  plt.title(y_test[i])
+  plt.gray()
+  ax.get_xaxis().set_visible(False)
+  ax.get_yaxis().set_visible(False)
+plt.show()
+
+# Initialize the image classifier.
+clf = ak.ImageClassifier(overwrite=True, max_trials=1)
+# Feed the image classifier with training data.
+clf.fit(x_train, y_train, epochs=3)
+
+
+# Export as a Keras Model
+model = clf.export_model()
+print(type(model.summary()))
+
+# Predict with the best model.
+predicted_y = clf.predict(x_test)
+print(predicted_y[:10])
+
+
+# Evaluate the best model with testing data.
+print(clf.evaluate(x_test, y_test))
+
+clf.fit(
+    x_train,
+    y_train,
+    # Split the training data and use the last 15% as validation data.
+    validation_split=0.15,
+    epochs=10,
+)
